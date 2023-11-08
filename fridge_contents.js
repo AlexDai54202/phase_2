aa9457f343ba977a1a79140841e4823634ff4b30
AlexDai54202 <jalexdai54202@gmail.com>
2023-11-08 00:20:12 -0500

    Added Filters

--- fridge_contents.js
+++ fridge_contents.js
@@ -26,12 +26,12 @@ LAYER_2_REF = 2
 LAYER_3_REF = 3
 
 class foodItem {
-    constructor(name, icon, amount, position){
+    constructor(name, icon, amount, position, date_of_purchase){
         this.name = name
         this.icon = icon.icon;
         this.amount = amount;
         this.position = position;
-        this.date_of_purchase = "2023-11-07"
+        this.date_of_purchase = date_of_purchase;
     }
     
     removeamount(amt) {
@@ -52,13 +52,13 @@ FRIDGE_CONTENTS = JSON.parse(localStorage.getItem("Fridge_Contents")); // null i
 function initialize() {
     // initialize fridge contents
     FRIDGE_CONTENTS = []
-    FRIDGE_CONTENTS.push(new foodItem("Salmon",fish,4,FREEZER))
-    FRIDGE_CONTENTS.push(new foodItem("Trout",fish,3,FREEZER))
-    FRIDGE_CONTENTS.push(new foodItem("Bass",fish,2,FREEZER))
-    FRIDGE_CONTENTS.push(new foodItem("Pork",meat,3,FREEZER))
-    FRIDGE_CONTENTS.push(new foodItem("Steak",meat,5,FREEZER))
-    FRIDGE_CONTENTS.push(new foodItem("Yogurt",dairy,3,LAYER_3_REF))
-    FRIDGE_CONTENTS.push(new foodItem("Milk",dairy,1,LAYER_3_REF))
+    FRIDGE_CONTENTS.push(new foodItem("Salmon",fish,4,FREEZER,new Date()))
+    FRIDGE_CONTENTS.push(new foodItem("Trout",fish,3,FREEZER,new Date()))
+    FRIDGE_CONTENTS.push(new foodItem("Bass",fish,2,FREEZER,new Date()))
+    FRIDGE_CONTENTS.push(new foodItem("Pork",meat,3,FREEZER,new Date()))
+    FRIDGE_CONTENTS.push(new foodItem("Steak",meat,5,FREEZER,new Date()))
+    FRIDGE_CONTENTS.push(new foodItem("Yogurt",dairy,3,LAYER_3_REF,new Date()))
+    FRIDGE_CONTENTS.push(new foodItem("Milk",dairy,1,LAYER_3_REF,new Date()))
 
     localStorage.setItem("Initialized", true)
     localStorage.setItem("Fridge_Contents",JSON.stringify(FRIDGE_CONTENTS))
@@ -78,7 +78,8 @@ function load_fridge_contents(FRIDGE_CONTENTS) {
         img.src = FRIDGE_CONTENTS[i].icon;
 
         var div = document.createElement("div");
-        div.className="smallsquare"
+        div.className="smallsquare foodItem"
+        div.setAttribute("dop", FRIDGE_CONTENTS[i].date_of_purchase)
         div.appendChild(img)
         div.appendChild(lab)
         div.setAttribute('onclick',"open_editor(\""+FRIDGE_CONTENTS[i].name+"\", "+FRIDGE_CONTENTS[i].amount+", "+FRIDGE_CONTENTS[i].position+")")
@@ -90,7 +91,6 @@ function load_fridge_contents(FRIDGE_CONTENTS) {
 }
 
 function open_editor(name, amt, layer) {
-    console.log(name, amt, layer)
     let i = 0;
     let item = -1;
     while(i<FRIDGE_CONTENTS.length) {
@@ -109,10 +109,9 @@ function open_editor(name, amt, layer) {
     
     modal.children[1].children[0].setAttribute('onclick',"remove_item(\"" + name + "\","+layer+")");
 
-    modal.style.display = "block";
-    
+    modal.children[0].children[4].value = new Date(FRIDGE_CONTENTS[item].date_of_purchase).toISOString().split('T')[0]
 
-    console.log(item)
+    modal.style.display = "block";
 }
 
 function remove_item(name, layer) {
@@ -150,6 +149,7 @@ function add_item(name, layer, amt) {
 
 var modal = document.getElementById("myModal");
 var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2");
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     FRIDGE_CONTENTS = FRIDGE_CONTENTS.filter(function (entry) {
@@ -157,7 +157,7 @@ window.onclick = function(event) {
     })
     localStorage.setItem("Fridge_Contents",JSON.stringify(FRIDGE_CONTENTS));
     
-    if (event.target == modal || event.target == modal1) {
+    if (event.target == modal || event.target == modal1 || event.target == modal2) {
         modal.style.display = "none";
         
         // todo: update localstorage with fridge data, refresh page.
@@ -182,6 +182,47 @@ function select_icon(enu) {
     }
 }
 
+function select_icon_filter(enu) {
+    let i = 0;
+    while (i < modal2.children[0].children[1].children.length) {
+        if (i == enu) {
+            if (modal2.children[0].children[1].children[i].style.backgroundColor == '') {
+                modal2.children[0].children[1].children[i].style.backgroundColor = '#90EE90';
+            } else {
+                modal2.children[0].children[1].children[i].style.backgroundColor = '';
+            }
+        }
+        i++;
+    }
+}
+
+function done_filtering() {
+    ok_icons = []
+    let i = 0
+    while (i < modal2.children[0].children[1].children.length) {
+        if (modal2.children[0].children[1].children[i].style.backgroundColor != '') {
+            ok_icons.push(modal2.children[0].children[1].children[i].children[0].getAttribute("src"))
+        }
+        i++;
+    }
+    console.log(ok_icons)
+    ok_date = new Date(modal2.children[0].children[3].value)
+
+    items = document.getElementsByClassName("foodItem")
+    console.log(items)
+    i = 0;
+    while (i < items.length) {
+        if (ok_icons.includes(items[i].children[0].getAttribute("src")) && (new Date(items[i].getAttribute("dop"))) <= ok_date) {
+            items[i].style.display = ""
+        } else {
+            items[i].style.display = "none"
+        }
+        i++;
+    }
+    modal2.style.display = "none"
+}
+
+
 function sumbit_form() {
     let i = 0;
     let icon = "";
@@ -194,19 +235,24 @@ function sumbit_form() {
     }
     amount = parseInt(modal1.children[0].children[5].children[1].textContent);
     item_name = modal1.children[0].children[3].value;
-    
+    date = modal1.children[0].children[7].value;
     if (amount > 0) {
-        FRIDGE_CONTENTS.push(new foodItem(item_name, {'icon':icon}, amount, LAYER_TO_ADD));
+        FRIDGE_CONTENTS.push(new foodItem(item_name, {'icon':icon}, amount, LAYER_TO_ADD, new Date(date)));
     }
     localStorage.setItem("Fridge_Contents",JSON.stringify(FRIDGE_CONTENTS))
     location.reload();
 }
 
+function open_filter() {
+    modal2.style.display = "block"
+    modal2.children[0].children[3].value = new Date().toISOString().split('T')[0]
+}
 
 
 var LAYER_TO_ADD = 0;
 function add_content_to_fridge(layer) {
     modal1.style.display = "block";
+    modal1.children[0].children[7].value = new Date().toISOString().split('T')[0]
     LAYER_TO_ADD = layer
 }  
 
