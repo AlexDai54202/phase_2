1cf2c2c3aabe07972e96df841828d5421900d499
briforreal <119989122+briforreal@users.noreply.github.com>
2023-11-09 12:21:42 -0500

    Update recipe_script.js

--- recipe_script.js
+++ recipe_script.js
@@ -243,3 +243,19 @@ function showJane(){
     vegRecipes.style.display = "none";
     janesFaves.style.display = "block";
 }
+
+function goto_home_page() {
+    location.href = './home_screen.html';
+}
+function goto_list_page() {
+    location.href = './shopping_list.html';
+}
+function goto_fridge_page() {
+    location.href = './fridge_contents.html';
+}
+function goto_profile_page() {
+    location.href = './profile.html';
+}
+function goto_recipe_page() {
+    location.href = './recipeList.html';
+}
