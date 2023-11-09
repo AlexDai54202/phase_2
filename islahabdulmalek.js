65dec468a04ee2539b9abc009047d56d653835e8
islahabdu <149726706+islahabdu@users.noreply.github.com>
2023-11-09 15:24:32 -0500

    Update islahabdulmalek.js

--- islahabdulmalek.js
+++ islahabdulmalek.js
@@ -1,6 +1,22 @@
+function goto_home_page() {
+    location.href = './home_screen.html';
+}
+function goto_list_page() {
+    location.href = './shopping_list.html';
+}
+function goto_fridge_page() {
+    location.href = './fridge_contents.html';
+}
+function goto_profile_page() {
+    location.href = './profile.html';
+}
+function goto_recipe_page() {
+    location.href = './recipeList.html';
+}
 
 const toggleBtn = document.querySelector('#toggleBtn');
 const divList = document.querySelector('.listHolder');
+
 
 
 toggleBtn.addEventListener('click', () => {
@@ -12,6 +28,7 @@ toggleBtn.addEventListener('click', () => {
     toggleBtn.innerHTML = 'Show List';
   }
 });
+
 
 const addInput = document.querySelector('#addInput');
 const addBtn = document.querySelector('#addBtn');
@@ -46,10 +63,21 @@ const listUl = document.querySelector('.list');
 const lis = listUl.children;
 
 function createBtn(li) {
-
+  // create remove button
   const remove = document.createElement('button');
   remove.className = 'btn-icon remove';
   li.appendChild(remove);
+
+   
+   const down = document.createElement('button');
+   down.className = 'btn-icon decrease';
+   li.appendChild(down);
+ 
+
+   const up = document.createElement('button');
+   up.className = 'btn-icon increase';
+   li.appendChild(up);
+
   return li;
 }
 
@@ -58,6 +86,8 @@ for (var i = 0; i < lis.length; i++) {
   createBtn(lis[i]);
 }
 
+
+
 divList.addEventListener('click', (event) => {
   if (event.target.tagName === 'BUTTON') {
     const button = event.target;
@@ -65,6 +95,15 @@ divList.addEventListener('click', (event) => {
     const ul = li.parentNode;
     if (button.className === 'btn-icon remove') {
       ul.removeChild(li);
-    }
+    }else if (button.className === 'btn-icon increase') {
+        myAmount = myAmount + 1;
+        var element = document.getElementById("amount");
+        element.innerHTML = myAmount;
+      } else if (button.className === 'btn-icon up') {
+        const prevLi = li.previousElementSibling;
+        if (prevLi) {
+          ul.insertBefore(li, prevLi);
+        }
+      }
   }
 });
